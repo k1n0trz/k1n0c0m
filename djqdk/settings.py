288438d3ec73006cea5bc4804c6469ad681fddf5
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-01-28 14:47:47 -0500

    static tres

--- djqdk/settings.py
+++ djqdk/settings.py
@@ -115,8 +115,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+import os
 
-STATIC_URL = "../static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "static/"
 
 LOGIN_URL = '/signin'
 
